828189131921bc348705762a1715111fa8c20acf
Derrick Lui <derricklui1@gmail.com>
2014-09-17 08:42:35 -0400

    Added classwork files

---
+++ classwork/Driver.java
@@ -0,0 +1,14 @@
+public class Driver {
+    public static void main(String[] args){
+	Greeter g1 = new Greeter();
+	Greeter g2 = new Greeter();
+	g1.greet();
+	g2.greet();
+
+	g1.setGreeting(new String("Sup!");
+
+	g1.greet();
+	g2.greet();
+    }
+}
+	   
