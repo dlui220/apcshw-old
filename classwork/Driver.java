f40edcae916fcd033eb345e698d8d00be0cc9336
Derrick Lui <derricklui1@gmail.com>
2014-09-18 08:40:08 -0400

    Wtf am I doing

--- classwork/Driver.java
+++ classwork/Driver.java
@@ -2,13 +2,24 @@ public class Driver {
     public static void main(String[] args){
 	Greeter g1 = new Greeter();
 	Greeter g2 = new Greeter();
-	g1.greet();
+
+	System.out.println(g1.greet());
+	System.out.println(g2.greet());
+
+	g1.setGreeting(new String("Sup!"));
+	// makes a new block of memory, puts "Sup!" in it and returns
+	// it's address
+	g1.greet();     
 	g2.greet();
 
-	g1.setGreeting(new String("Sup!");
-
+	g2.setGreeting("Howdy!");
+	// string literal returns address without grabbing a new block
+	// of memory. If a bunch of variables ask for g2 they will
+	// refer to the same string instead of referring to different
+	// newly created strings.
 	g1.greet();
 	g2.greet();
+	System.out.println("g1's greeting is"+s);
     }
 }
 	   
--- classwork/Greeter.java
+++ classwork/Greeter.java
@@ -1,9 +1,35 @@
 public class Greeter {
+    // instance variables are almost always private
+    // we access them with "accessor methods" get and set
     private String greeting = new String("Hello world!");
     public void setGreeting(String s) {
 	greeting = s;
     }
 
+    public Greeter(String g) {
+	System.out.println("In the constructor");
+	greeting = g;
+    }
+    // constructors ^^
+    /*
+      Notes:
+        - the name is the same as the class name
+	- there is no return type (nothing between public and the name)
+	- it's ALWAYS public
+
+	a constructor is run automatically when yo instantiate the class. 
+	that is, if you have: Greeter g = new Greeter("Hello")
+	then Java will automatically call this constructor sending Hello in
+	as parameter g. It will then set the instance variable
+	greeting to what g refers to, which is hello.
+    */
+    public String getGreeting() {
+	return greeting;
+    }
+    // String is the "return type" that is, the type of the data that we
+    // will send back using the return statement.
+    // void is a special "return type" which means we won't be returning
+    // any value.
     public void greet() {
 	//uncomment the following line for question 2
 	//String greeting = new String("Sup!");
