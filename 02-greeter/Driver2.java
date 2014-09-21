8fc19d19ea5c44ae3336a21fe90bfea935beae65
Derrick Lui <derricklui1@gmail.com>
2014-09-21 13:09:05 -0700

    Added Driver.java for the strings HW

--- 02-greeter/Driver2.java
+++ 02-greeter/Driver2.java
@@ -1,25 +1,15 @@
-public class Driver2 {
-    public static void main(String[] args) {
+public class Driver2{
 
-	Greeter g1 = new Greeter();
-	// the above line is the same as 
-	// Greeter g1;
-	// g1 = new Greeter();
-	
-	Greeter g2,g3;
-	g2 = new Greeter();
+    public static void main(String[] args){
+	Greeter g1 = new Greeter("Hello");
+	System.out.println(g1.greet());
+	System.out.println("Before the contsructor is called");
+	Greeter g2 = new Greeter("Howdy!");
+	System.out.println("After the contsructor is called");
+	System.out.println(g2.greet());
 
-	g3 = g1;
-
-	g1.greet();
-	g2.greet();
-	g3.greet();
-
-	// make a new greeter and set g1 to refer to it
-	// Note: this does not re-assign g3.
-	System.out.println("g1: "+g1);
-	System.out.println("g2: "+g2);
-	System.out.println("g3: "+g3);
-
+	System.out.println(g1.greetPerson("Tom"));
+	System.out.println(g1.greetPerson("Sarah"));
+	System.out.println(g1.loudGreeting());
     }
 }
---
+++ 03-strings/Driver.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Driver {
+    public static void main(String[] args){
+        String s="Greatest Ever";
+        int index = s.indexOf(" ");
+	//	System.out.println(s);
+	//	System.out.println(index);
+	String firstname = s.substring(0,index);
+	String lastname = s.substring(index+1);
+	System.out.println("Last name: "+lastname);
+	System.out.println("First name: "+firstname);
+
+    }
+}
