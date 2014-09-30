01d1d5f2620c83fb949c586e595c85ba6be2c810
Derrick Lui <derricklui1@gmail.com>
2014-09-30 08:42:47 -0400

    Added Driver

---
+++ 06-loops/Driver.java
@@ -0,0 +1,15 @@
+public String frontTimes(String str, int n) {
+  if (str.length()<=3){
+    String front = str;
+    while (n > 0) {
+    return front;
+    n=n-1;
+    }} else {
+    String front = str.substring(0,3);
+    while (n > 0) {
+    return front;
+    n=n-1;
+    }
+    }
+    
+    }
