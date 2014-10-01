65a39175e7ca2f48f622e3a7269b11e80df29b16
Derrick Lui <derricklui1@gmail.com>
2014-10-01 03:26:14 -0700

    Added MoreShapes.java file for homework.

--- 08-moreshapes/MoreShapes.java
+++ 08-moreshapes/MoreShapes.java
@@ -1,13 +1,31 @@
-public class MoreShapes {
-    
-    public String line(String a, int b) {
-	String result = new String();
-	for (int i=0;i<b;i++) {
-	    result+="*";
+public class MoreShapes{
+
+    public String line(String c, int n) {
+	String s = "";
+	for (int i=0;i<n;i++) {
+	    s=s+c;
 	}
-	return result;
+	return s;
+    }
+    
+    public String frameline(String c, int n) {
+	String s = c;
+	for (int i=0;i<n-2;i++){
+	    s = s + " ";
+	}
+	s = s + c;
+	return s;
     }
 
-    public String frame(int a, int b) {
+    
+    public String frame(int r, int c) {
+	String result = line("*",c)+"\n";
+	int height = r;
+	int width = c;
+	int i;
+	for (i=0;i<height-2;i++){
+	    result = result + frameline("*",width) + "\n";
+	}
+	return (result+line("*",c));
     }
 }
