24a72370cae285438b8dd3bd3ec4c9c6898cb69b
Derrick Lui <derricklui1@gmail.com>
2014-10-02 08:04:34 -0400

    Moved to 07-shapes directory.

---
+++ 07-shapes/Shapes.java
@@ -0,0 +1,67 @@
+
+
+public class Shapes {
+
+    public String box(int r, int c){
+	String s= "";
+	int row = 0;
+	int col;
+	while (row<r){
+	    col = c;
+	    while (col>0){
+		s = s + "*";
+		col=col-1;
+	    }
+	    s=s+"\n";
+	    row=row+1;
+	}
+	return s;
+    }
+    // Worked with Lulu on this in class
+    public String tri1( int h ) {
+	int x = 1;
+	String end = "";
+	while ( x <= h ) {
+	    int y = 0;
+	    while ( y < x ) {
+		end = end + "*";
+		y = y + 1;
+	    }
+	    end = end + "\n";
+	    x = x + 1;
+	}
+	return end;
+    }
+
+    public String tri2(int h) {
+	String result = "\n";
+	int newh = h;
+	int x = h - 1;
+	while (x > 0) {
+	    int c = 0;
+	    while (c < x) {
+		int g = h;
+		result = result + " ";
+		c = c + 1;
+	    }
+	    x = x - 1;
+	    if (result.substring(h-1) != "*") {
+		while (result.length() % (newh + 1) != 0) {
+		    result = result + "*";
+		}
+	    } else {
+		result = result + "\n" ;
+	    }
+	    result = result + "\n";
+	}
+	while (h>0){
+	    h = h - 1;
+	    result = result + "*";
+	}
+	return result;
+    }
+
+
+	
+
+}
--- classwork/Box/Driver.java
+++ classwork/Box/Driver.java
@@ -1,6 +1,6 @@
 public class Driver {
     public static void main(String[] args) {
 	Box g1 = new Box();
-	System.out.println(g1.box(3,4));
+	System.out.println(g1.box(8,20));
 	    }
 }
