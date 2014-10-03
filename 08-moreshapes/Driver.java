a5158343f62dbc1999d4131de97c36dcf23e59ff
Derrick Lui <derricklui1@gmail.com>
2014-10-03 08:37:48 -0400

    MoreShapes added

---
+++ 08-moreshapes/Driver.java
@@ -0,0 +1,6 @@
+public class Driver {
+    public static void main(String[] args) {
+	MoreShapes s = new MoreShapes();
+	System.out.println(s.line("*",5));
+    }
+}
---
+++ 08-moreshapes/MoreShapes.java
@@ -0,0 +1,13 @@
+public class MoreShapes {
+    
+    public String line(String a, int b) {
+	String result = new String();
+	for (int i=0;i<b;i++) {
+	    result+="*";
+	}
+	return result;
+    }
+
+    public String frame(int a, int b) {
+    }
+}
