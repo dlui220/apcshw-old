ebd84e732921a3d9a4bc491ca13f415acbe8ca14
Derrick Lui <derricklui1@gmail.com>
2014-10-27 08:41:39 -0400

    added ArrayStuff file

---
+++ 14-arrays/ArrayStuff.java
@@ -0,0 +1,11 @@
+public class ArrayStuff {
+    public int find(int[] array, int n){
+	int result = 0;
+	for (int i=0;i<array.length;i++){
+	    if (int[i] == n) {
+		result = i;
+	    }
+	    return result;
+	}
+    }
+}
