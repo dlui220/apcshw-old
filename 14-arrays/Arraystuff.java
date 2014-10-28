2b4789d31e0d6faeac3ec64fad5edb6b77cbdee0
Derrick Lui <derricklui1@gmail.com>
2014-10-27 18:04:57 -0700

    Added homework methods

--- 14-arrays/Arraystuff.java
+++ 14-arrays/Arraystuff.java
@@ -1,17 +1,13 @@
 import java.util.*;
 
 public class Arraystuff {
-    /*-------------------- Instance Variables --------------------*/
-
     private int[] a;
     private Random rnd;
     /* by making a variable final we can't change
        the value after the initial asssignment 
     */
     public final int final_example = 123;
-
-    /*-------------------- Constructors --------------------*/
-    
+ 
     public Arraystuff(int n){
 	rnd = new Random();
 	a = new int[n];
@@ -24,7 +20,6 @@ public class Arraystuff {
 	this(100);
     }
 
-    /*-------------------- Methods --------------------*/
     public String toString(){
 	String s="";
 	for (int i=0;i<a.length;i++){
@@ -32,20 +27,41 @@ public class Arraystuff {
 	}
 	return s;
     }
-    public static void main(String[] args) {
-	Arraystuff as = new Arraystuff();
-	System.out.println(as);
-	
-	
-    }
 
     public int find(int n){
 	int result = 0;
+	boolean found = false;
 	for (int i=0;i<a.length;i++){
-	    if (a[i] == n) {
-		result = i;
+	    if (!found){
+		if (a[i] == n) {
+		    result = i + 1;
+		    found = true;
+		} else { 
+		    result = -1;
+		}	    
 	    }
-	    return result;
 	}
+	return result;
+	
     }
+
+    public int maxVal(){
+	int result =a[0];
+	for (int i=1;i<a.length;i++){
+	    if (a[i]>result){
+		result = a[i];
+	    }
+	}
+	return result;
+    }
+
+    public static void main(String[] args) {
+	Arraystuff as = new Arraystuff();
+	System.out.println(as);
+	System.out.println(as.find(100));
+	System.out.println(as.maxVal());
+	
+    }
+
+
 }
