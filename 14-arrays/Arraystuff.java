00920c5d1a1b4e993eb93dd3813d5ef90dae34cb
Derrick Lui <derricklui1@gmail.com>
2014-10-28 18:38:02 -0700

    Fixed more14 :)

--- 14-arrays/Arraystuff.java
+++ 14-arrays/Arraystuff.java
@@ -87,7 +87,7 @@ public class Arraystuff {
     public boolean more14(int[] nums) {
 	int count1 = 0;
 	int count4 = 0;
-	for (int i=1;i<nums.length;i++){
+	for (int i=0;i<nums.length;i++){
 	    if (nums[i] == 1) {
 		count1++;
 	    }
