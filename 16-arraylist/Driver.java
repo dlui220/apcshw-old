d2cafecb48208dba99ac726dba4280ef3bc0e565
Derrick Lui <derricklui1@gmail.com>
2014-11-12 08:41:11 -0500

    ArrayList started

--- 16-arraylist/Driver.java
+++ 16-arraylist/Driver.java
@@ -2,9 +2,9 @@ import java.io.*;
 import java.util.*;
 
 public class Driver {
-    ArrayList<Integer> al = new ArrayList();
+    ArrayList<Integer> al = new ArrayList<Integer>();
+    Random r = new Random();
     for (int i = 0;i<20;i++) {
-	Random r = new Random();
 	al.add(r.nextInt(5));
 	System.out.println(al);
 	
