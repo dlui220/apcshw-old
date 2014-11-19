954888a1b993551753352df67bb8f9292050731d
Derrick Lui <derricklui1@gmail.com>
2014-11-19 08:11:04 -0500

    WordSearch added

--- classwork/WordSearch/WordSearch.java
+++ classwork/WordSearch/WordSearch.java
@@ -1,24 +1,45 @@
-public class WordSearch {
+public class WordSearch{
 
     private char[][] board;
 
     public WordSearch(int r, int c){
-	board=new char[r][c];
-	for (int i=0;i<board.length;i++) {
-	    for (int j=0
+	board = new char[r][c];
+	for (int i = 0; i < board.length; i++) {
+	    for (int j = 0; j < board[i].length; j++) {
+		board[i][j]='.';
+	    }
+	}
+				
     }
     public WordSearch(){
-	this(30,20);
+	this(20,30);
     }
 
     public String toString(){
-	String s="";
-	for (int i=0; i<board.length;i++){
-	    for (int j=0;j<board[i].length;j++){
-		s=s+board[i][j];
+	String s = "";
+	for (int i = 0; i < board.length; i++) {
+	    for (int j = 0; j < board[i].length; j++) {
+		s = s + board[i][j];
 	    }
-	    s=s+"\n";
+	    s = s + "\n";
 	}
 	return s;
+    }
+
+    public void addWordH(String w,int row,int col){
+	int r = row, c = col;
+	
+	for (int i=0;i<w.length();i++) {
+	    board[r][c] = w.charAt(i);
+	    c++;
+	}
+	    
+    }
+
+    public static void main(String[] args) {
+	WordSearch w = new WordSearch();
+	System.out.println(w);
+	w.addWordH("hello",3,5);
+	System.out.println(w);
     }
 }
