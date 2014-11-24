eddbf3189a966f0a746ebfb530dddd6a6df921f2
Derrick Lui <derricklui1@gmail.com>
2014-11-23 19:18:35 -0800

    Updated

--- 18-WordSearch/WordSearch.java
+++ 18-WordSearch/WordSearch.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.util.*;
+
 public class WordSearch{
 
     private char[][] board;
@@ -68,55 +71,91 @@ public class WordSearch{
     public void addDUpR(String s, int row, int col) {
 	int r = row, c = col;
 	
-	for(int i=0; i<s.length();i++) {
-	    board[r][c] = s.charAt(i);
-	    r++;
-	    c++;
-	    if(c > board[row].length || r > board.length) {break;}
+	if(r+s.length() <= board.length && c+s.length() <=board[r].length) {
+	    for(int i=0; i<s.length();i++) {
+		board[r][c] = s.charAt(i);
+		r++;
+		c++;
+	    }
 	}
     }
 
     public void addDUpL(String s, int row, int col) {
 	int r = row, c = col;
-	
-	for(int i=0; i<s.length();i++) {
-	    board[r][c] = s.charAt(i);
-	    r++;
-	    c--;
+
+	if(r+s.length() <= board.length && c-s.length() >= 0) {
+	    for(int i=0; i<s.length();i++) {
+		board[r][c] = s.charAt(i);
+		r++;
+		c--;
+	    }
 	}
     }
-
     public void addDDownR(String s, int row, int col) {
 	int r = row, c = col;
 	
-	for(int i=0; i<s.length();i++) {
-	    board[r][c] = s.charAt(i);
-	    r--;
-	    c++;
+	if(r-s.length() >=0 && c+s.length() <= board[r].length) {
+	    for(int i=0; i<s.length();i++) {
+		board[r][c] = s.charAt(i);
+		r--;
+		c++;
+	    }
 	}
     }
 
     public void addDDownL(String s, int row, int col) {
 	int r = row, c = col;
-	
-	for(int i=0; i<s.length();i++) {
-	    board[r][c] = s.charAt(i);
-	    r--;
-	    c--;
+
+	if(r-s.length() >= 0 && c-s.length() >= 0) {
+	    for(int i=0; i<s.length();i++) {
+		board[r][c] = s.charAt(i);
+		r--;
+		c--;
+	    }
 	}
     }
+
+    public boolean addWord(String W){
+	Random r = new Random();
+	char[][] testboard = board;
+	int randomc = r.nextInt(board[0].length);
+	int randomr = r.nextInt(board.length);
+	int x = r.nextInt(8);
+
+	if (x == 0){
+	    addWordH(W,randomr,randomc);
+	} else if (x == 1) {
+	    addWordHBackward(W,randomr,randomc);
+	} else if (x == 2) {
+	    addWordV(W,randomr,randomc);
+	} else if (x == 3) {
+	    addWordVBackward(W,randomr,randomc);
+	} else if (x == 4) {
+	    addDUpR(W,randomr,randomc);
+	} else if (x == 5) {
+	    addDUpL(W,randomr,randomc);
+	} else if (x == 6) {
+	    addDDownR(W,randomr,randomc);
+	} else if (x == 7) {
+	    addDDownL(W,randomr,randomc);
+	}
+	if (testboard.equals(board)){
+	    return false;
+	}else{
+	    return true;
+	}
+    }
+       
+
     public static void main(String[] args) {
 	WordSearch w = new WordSearch();
 	try {
-	System.out.println(w);
-	w.addWordH("hello",1,5);
-	w.addWordHBackward("hello",4,6);
-	w.addWordV("HELLO",6,1);
-	w.addWordVBackward("whoaa",7,10);
-	w.addDUpL("zamansky",5,5);
-	System.out.println(w);
+	    for (int i=0;i<100;i++) {
+		w.addWord("Wow");
+	    }
+	    System.out.println(w);
 	} catch (Exception e) {
-	    System.out.println("Your error is: "+e);
+	    System.out.println(e);
 	}
 
     }
