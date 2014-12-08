e1f02bb0c633f36198b67fc5bf7dac2769b9cefc
Derrick Lui <derricklui1@gmail.com>
2014-12-08 08:03:59 -0500

    updated

--- 20-sorting/Driver.java
+++ 20-sorting/Driver.java
@@ -16,31 +16,43 @@ public class Driver{
 
 	    // Regular Sarray after this point
 	    sarray s = new sarray();
-	    System.out.println();
-	    System.out.println("Regular Sarray: ");
-	    System.out.println(s.toString());
-	    System.out.println(s.size());
+	    sarray r = new sarray();
 	    for (int i=0;i<10;i++)  {
 		s.add(i,"JEANNEEEE");
+		r.add(i,"jennnnnn");
 	    }
+	    // for s
 	    s.set(5,"DERRICK");
 	    s.set(8,"ZAMANSKY");
 	    s.set(2,"CAR");
 	    s.set(7,"APPLE");
 	    s.set(9,"EUREKA");
-	    System.out.println(s.toString());
+	    // for r
+	    r.set(5,"DERRICK");
+	    r.set(8,"ZAMANSKY");
+	    r.set(2,"CAR");
+	    r.set(7,"APPLE");
+	    r.set(9,"EUREKA");
+	    
+	    
+
+	   
+
+
+	    long startTime = System.nanoTime();
 	    s.ssortAscending();
-	    System.out.println(s.toString());
-	    s.ssortDescending();
-	    System.out.println(s.toString());
-	    
-	    
-	    
+	    long endTime = System.nanoTime();
+	    System.out.println("ssort took " + ((endTime - startTime)*.000001) + " milliseconds");
+
+	    long startTime2 = System.nanoTime();
+	    r.isort();
+	    long endTime2 = System.nanoTime();
+	    System.out.println("isort took " + ((endTime2 - startTime2)*.000001) + " milliseconds");
 	} catch (ArrayIndexOutOfBoundsException e) {
 	    System.out.println("An error has occured: "+e);
 	}
 
-	
+	// isort is a lot faster
     }
 	
 	
